6e9b8f92df5a55d636c48051452025e35b8881ec
ecabigting <ericcabigting@outlook.com>
2023-02-03 17:00:18 +0400

    update folder structure

--- Password_Manager/pw_mgr.py
+++ Password_Manager/pw_mgr.py
@@ -8,10 +8,15 @@ def add():
     uname = input("Set Username: ")
     pword = input("Set Password: ")
 
-    # set the open mode to "a" append
+    # set the open mode to "a" append, add the
     # declare it as "f" to automatically handle open and closing of
     # file instead of declaring it as a file that manually opens and needs closing
+    # additional parameters instead of "a"
+    # "w" for write, override if file exist
+    # "r" only open as read-only, throws an error if file does not exist
+    # "a" append mode, add to the end of file, or create the file if it does not exist
     with open("plist.txt","a") as f:
+        f.write(label + "|" + uname + "|" + "|" + pword)
         
 
 while True:
